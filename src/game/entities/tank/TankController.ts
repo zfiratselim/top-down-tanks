c1aabb4f27396cd22cffb0c9caec9d9e59d50bcb
zfiratselim <zirekoglufs@gmail.com>
2025-02-11 16:35:02 +0300

    added shoot and addExplosion func in TankController

--- src/game/entities/tank/TankController.ts
+++ src/game/entities/tank/TankController.ts
@@ -7,14 +7,46 @@ import { BulletType, TankType } from "../../../types/Enum";
 
 
 export default class TankController extends PIXI.Container {
+  Tank: Tank;
+  Shot: Shot;
+  Bullet: Bullet;
+  Explosion: Explosion;
   constructor(x: number, y: number) {
     super();
     this.x = x;
     this.y = y;
-    const tank = new Tank(TankType.red, 0, 0);
-    const shot = new Shot(32, 0);
-    const bullet = new Bullet(BulletType.red, 100, 0, Math.PI / 2)
-    const explosion = new Explosion(300, 0);
-    this.addChild(shot, bullet, tank, explosion)
+    this.Tank = new Tank(TankType.red, 0, 0);
+    this.Shot = new Shot(0, 0);
+    this.Explosion = new Explosion(-200, -200)
+    this.Bullet = new Bullet(BulletType.red, 0, 0, Math.PI / 2)
+    this.addChild(this.Shot, this.Bullet, this.Tank, this.Explosion);
+    this.shoot(300, 0);
+  }
+  shoot(x: number, y: number) {
+    this.Bullet.x = this.Tank.x;
+    this.Bullet.y = this.Tank.y;
+
+    const deltaX = (x - this.Bullet.x) / 20;
+    const deltaY = (y - this.Bullet.y) / 20;
+
+    const interval = setInterval(() => {
+      this.Bullet.x += deltaX;
+      this.Bullet.y += deltaY
+      if (this.Bullet.x == x) {
+        clearInterval(interval);
+        this.Bullet.x = -200;
+        this.Bullet.y = -200;
+        this.addExplosion(x, y);
+      }
+    }, 50)
+  }
+  addExplosion(x: number, y: number) {
+    this.Explosion.x = x;
+    this.Explosion.y = y;
+
+    setTimeout(() => {
+      this.Explosion.x = -200;
+      this.Explosion.y = -200;
+    }, 2000);
   }
 }
